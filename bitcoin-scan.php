52b368856b5803ccd08c52ece311a9f440d257e7
Matt Corallo <matt@bluematt.me>
2011-07-02 13:49:31 +0200

    Better now.

--- bitcoin-scan.php
+++ bitcoin-scan.php
@@ -19,7 +19,7 @@ try {
 	$origNode = new Bitcoin\Node($arr[0], $port, $CONFIG['CONNECT_TIMEOUT']);
 	$nodes = $origNode->getAddr();
 
-        if (!empty($nodes) {
+        if (!empty($nodes)) {
 		start_db_transaction();
 		if ($port == 8333)
 			add_node_to_dns($arr[0], $origNode->getVersion());
@@ -28,6 +28,10 @@ try {
 			if ($node["services1"] == 1 && $node["services2"] == 0 && $node["timestamp"] >= time() - $CONFIG['MIN_LAST_SEEN'])
 				add_untested_node($node["ipv4"], $node["port"]);
 		}
+		commit_db_transaction();
+	}else{
+		start_db_transaction();
+		remove_node($arr[0], $port);
 		commit_db_transaction();
 	}
 } catch (Exception $e) {

d8363e14dea0d595ec2f599f1c821cb977de0150
Matt Corallo <matt@bluematt.me>
2011-07-13 22:08:40 +0200

    fix

--- count-nodes.php
+++ count-nodes.php
@@ -12,11 +12,11 @@ while($row = get_assoc_result_row($result)) {
 }
 $result = query_dns_total();
 $result = init_results($result);
-$row = get_assoc_result_row($result)
+$row = get_assoc_result_row($result);
 echo "In DNS:\t\t".$row['COUNT(*)']."\n";
 $result = query_total();
 $result = init_results($result);
-$row = get_assoc_result_row($result)
+$row = get_assoc_result_row($result);
 echo "Total:\t\t".$row['COUNT(*)']."\n";
 ?>
 

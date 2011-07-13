02649004970ea11bfabfe23e29f93601b529a059
Matt Corallo <matt@bluematt.me>
2011-07-13 22:10:09 +0200

    better ui

--- count-nodes.php
+++ count-nodes.php
@@ -13,10 +13,10 @@ while($row = get_assoc_result_row($result)) {
 $result = query_dns_total();
 $result = init_results($result);
 $row = get_assoc_result_row($result);
-echo "In DNS:\t\t".$row['COUNT(*)']."\n";
+echo "In DNS:\t".$row['COUNT(*)']."\n";
 $result = query_total();
 $result = init_results($result);
 $row = get_assoc_result_row($result);
-echo "Total:\t\t".$row['COUNT(*)']."\n";
+echo "Total:\t".$row['COUNT(*)']."\n";
 ?>
 
